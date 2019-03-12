6718233c7142a57dd5263153a178c131feda4a72
whoami90506 <whoami90506@github.com>
2019-03-12 23:18:09 +0800

    add Makefile

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include<fstream>
 #include<vector>
+using namespace std;
+
 
 int main(int argc, char** argv){
-
+    if(argc < 2 || argc > 3){
+        cout << "Usage: exec [input_file_name] <output_file_name>\n";
+        return 1;
+    }
 }
