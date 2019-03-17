f38a58e30a29ad9b736071fb8f1353e3ce425c92
whoami90506 <whoami90506@github.com>
2019-03-18 07:47:07 +0800

    finish calculate

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 short alpha, beta;
@@ -47,10 +48,38 @@ bool readfile(ifstream& ifs){
 
 
 int calculate(){
+    int result = 0;
     int **V = new int*[seqA.size()];
     int **E = new int*[seqA.size()];
     int **F = new int*[seqA.size()];
-    for(int i = 0; i < seqA.size(); ++i);
+    for(unsigned i = 0; i < seqA.size(); ++i){
+        V[i] = new int[seqB.size()];
+        E[i] = new int[seqB.size()];
+        F[i] = new int[seqB.size()];
+    }
+
+    for(unsigned i = 0; i < seqA.size(); ++i){
+        for(unsigned j = 0; j < seqB.size(); ++j){
+            E[i][j] = j ? max(V[i][j-1] - alpha, E[i][j-1] - beta) : 0;
+            F[i][j] = i ? max(V[i-1][j] - alpha, F[i-1][j] - beta) : 0;
+
+            V[i][j] = max(E[i][j], F[i][j]);
+            int temp = (i > 0 && j > 0) ? max(V[i-1][j-1] + tabal[seqA[i]] [seqB[j]], 0) : tabal[seqA[i]] [seqB[j]] ;
+            V[i][j] = max(V[i][j], temp);
+
+            result = max(V[i][j], result);
+        }
+    }
+
+    for(unsigned i = 0; i < seqA.size(); ++i){
+        delete [] V[i];
+        delete [] E[i];
+        delete [] F[i];
+    }
+    delete [] V;
+    delete [] E;
+    delete [] F;
+    return result;
 }
 
 int main(int argc, char** argv){
@@ -65,14 +94,16 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    while(readfile(ifs)){
-        for(auto& i:seqA)cout << i << ' ';
-        cout << endl;
-        for(auto& i:seqB)cout << i << ' ';
-        cout << endl;
-
-    }
-
+    //calculate
+    vector<int> result;
+    while(readfile(ifs))result.push_back(calculate() );
     ifs.close();
 
+    //output
+    for(auto &i : result)cout << i << endl;
+    if (argc == 3){
+        ofstream os(argv[2]);
+        for(auto &i : result)os << i << endl;
+        os.close();
+    }
 }
