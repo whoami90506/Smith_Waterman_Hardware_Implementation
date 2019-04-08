e3c8f8851f51a79f93e76fa287940862e5eb5949
whoami90506 <whoami90506@github.com>
2019-04-08 17:03:52 +0800

    add PRArray

---
+++ hardware/PEArray.v
@@ -0,0 +1,14 @@
+`ifdef PEARRAY
+`else
+`define PEARRAY
+
+`include "util.v"
+
+module PEArray (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+	
+);
+
+endmodule
+`endif
