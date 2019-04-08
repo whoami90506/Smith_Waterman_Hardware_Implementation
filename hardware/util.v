526bc806e41e1d92a6148dcfe40fae92c84ab45c
whoami90506 <whoami90506@github.com>
2019-04-08 12:59:14 +0800

    add DATA_WIDTH

--- hardware/util.v
+++ hardware/util.v
@@ -7,16 +7,16 @@
 	`define V_E_F_Bit  16
 	`define Match_bit  8
 
-	module myMax (
-		input  [`V_E_F_Bit-1 : 0 ] a,
-		input  [`V_E_F_Bit-1 : 0 ] b,
-		output [`V_E_F_Bit-1 : 0 ] result
+	module myMax #(parameter DATA_WIDTH = `V_E_F_Bit)(
+		input  [DATA_WIDTH-1 : 0 ] a,
+		input  [DATA_WIDTH-1 : 0 ] b,
+		output [DATA_WIDTH-1 : 0 ] result
 	);
 		wire compare, apbp, apbn, anbn;
-		assign compare = (a[`V_E_F_Bit-2:0] >= b[`V_E_F_Bit -2:0] );
-		assign apbp = (~a[`V_E_F_Bit -1]) & (~b[`V_E_F_Bit -1]);
-		assign apbn = (~a[`V_E_F_Bit -1]) & ( b[`V_E_F_Bit -1]);
-		assign anbn = ( a[`V_E_F_Bit -1]) & ( b[`V_E_F_Bit -1]);
+		assign compare = (a[DATA_WIDTH-2:0] >= b[DATA_WIDTH -2:0] );
+		assign apbp = (~a[DATA_WIDTH -1]) & (~b[DATA_WIDTH -1]);
+		assign apbn = (~a[DATA_WIDTH -1]) & ( b[DATA_WIDTH -1]);
+		assign anbn = ( a[DATA_WIDTH -1]) & ( b[DATA_WIDTH -1]);
 
 		assign chooseA = apbn | (apbp & compare) | (anbn & (~compare));
 		assign result = chooseA ? a : b;
