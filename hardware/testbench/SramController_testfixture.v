5c921dc795282557e295b346f3540cd801cd7607
whoami90506 <whoami90506@github.com>
2019-05-06 03:00:38 +0800

    wip

--- hardware/testbench/SramController_testfixture.v
+++ hardware/testbench/SramController_testfixture.v
@@ -1,7 +1,7 @@
 `timescale 1ns/10ps
 `define CYCLE    20           	        // Modify your clock period here
-`define TERMINATION  50000
-`define SEQUENCE
+`define TERMINATION  5000
+`define SEQUENCE "testbench/sram_t01.dat"
 
 `include "src/SramController.v"
 
@@ -19,18 +19,16 @@ wire [`Sram_Word-1 : 0] o_request_data;
 wire o_busy;
 wire [`Max_T_size_log-1 : 0] o_T_size;
 
-
-integer i, j, k, l, err;
-
-reg   [2:0]   cmd_mem   [0:CMD_N_PAT-1];
-
 SramController Top(.clk(clk), .rst_n(reset), .i_PE_request(i_PE_request), .o_request_data(o_request_data), 
 	.i_PE_send(i_PE_send), .i_send_data(i_send_data), .i_init(i_init), .i_start_read_t(i_start_read_t), 
 	.i_t(i_t), .o_busy(o_busy), .o_T_size(o_T_size));
 
-//initial $sdf_annotate(`SDFFILE, top);
-initial	$readmemh (`CMD,    cmd_mem);
-initial	$readmemh (`EXPECT, out_mem);
+integer num, err, stage;
+integer i;
+reg [17:0]   t_mem   [0:`Sram_Addr-1];
+reg [`Sram_Word-1 : 0] idx;
+
+initial	$readmemh (`SEQUENCE, t_mem);
 
 initial begin
 $fsdbDumpfile("sram.fsdb");
@@ -40,71 +38,80 @@ end
 
 initial begin
    clk         = 1'b0;
-   reset       = 1'b0;
-   over	       = 1'b0;
-   l	       = 0;
-   err         = 0;   
+   reset       = 1'b1;
+   err         = 0;
+   stage       = 0; 
+   idx         = 0; 
+   num         = 0; 
+   i           = 0;
+
+   i_PE_request = 1'd0;
+   i_PE_send = 1'd0;
+   i_send_data = 0;
+   i_init = 1'd0;
+   i_start_read_t = 1'd0;
+   i_t = 18'd0;
+
+   @(negedge o_busy);
+   stage = 1;
+   $display("=======");
+   $display("STAGE 1");
+   $display("=======");
 end
 
 always begin #(`CYCLE/2) clk = ~clk; end
 
 initial begin
-   @(negedge clk)  reset = 1'b1;
-   #t_reset        reset = 1'b0;
+   @(negedge clk)reset = 1'b0;
+   #2* `CYCLE    reset = 1'b1;
+   # `CYCLE;
+   @(posedge clk)i_start_read_t = 1'b1;
+   @(posedge clk)i_start_read_t = 1'b0;
                                   
 end  
 
 
-always @(negedge clk)
-begin
-
-	begin
-	if (l < CMD_N_PAT)
-	begin
-		if(!busy) 
-		begin
-        	cmd = cmd_mem[l];
-        	cmd_valid = 1'b1;
-		l=l+1;
-		end  
-		else
-		cmd_valid = 1'b0;
-	end
-	else
-	l=l;
+always @(posedge clk) begin
+	if(o_busy) begin
+		i_t = t_mem[num];
+		num = num +1;
 	end
 end
 
+function [`Sram_Word-1 : 0]T_to_ans;
+	input [17:0] in;
+	T_to_ans = {in[17:14], in[13:12], idx, idx, in[11:10], idx, idx, in[9:8], idx, idx, in[7:6], idx, idx,
+	 in[5:4], idx, idx, in[3:2], idx, idx, in[1:0], idx, idx};
+endfunction
 
-initial @(posedge done) 
-begin
-   for(k=0;k<64;k=k+1)begin
-         if( IRB_1.mem[k] !== out_mem[k]) 
-		begin
-         	$display("ERROR at %d:output %h !=expect %h ",k, IRB_1.mem[k], out_mem[k]);
-         	err = err+1 ;
+always @(posedge clk) begin
+	if(stage == 1) begin
+		i_PE_request = 1'd1;
+
+		if(o_request_data[`Sram_Word-1]) begin
+			if(o_request_data != T_to_ans(t_mem[i])) begin
+				$display("ERROR at %d: t_in %h, get %h ", i, t_mem[i], o_request_data);
+				err = err +1;
+			end
+			i = i+1;
+
+			if(i == num) begin
+				i = 0;
+				i_PE_request = 1'd0;
+				$finish;
+			end
 		end
-         else if ( out_mem[k] === 8'dx)
-                begin
-                $display("ERROR at %d:output %h !=expect %h ",k, IRB_1.mem[k], out_mem[k]);
-		err=err+1;
-                end   
- over=1'b1;
-end
-        begin
-	if (err === 0 &&  over===1'b1  )  begin
-	            $display("All data have been generated successfully!\n");
-	            $display("-------------------PASS-------------------\n");
-		    #10 $finish;
-	         end
-	         else if( over===1'b1 )
-		 begin 
-	            $display("There are %d errors!\n", err);
-	            $display("---------------------------------------------\n");
-		    #10 $finish;
-         	 end
-	
+
 	end
+end
+
+initial begin
+	#(`TERMINATION * `CYCLE);
+	$display("================================================================================================================");
+	$display("(/`n`)/ ~#  There is something wrong with your code!!"); 
+	$display("Time out!! The simulation didn't finish after %d cycles!!, Please check it!!!", `TERMINATION); 
+	$display("================================================================================================================");
+	#`CYCLE $finish;
 end
 
 endmodule
