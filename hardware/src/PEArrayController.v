2ccf20e767d170856cc3800ae5c0d801b4c619f8
whoami90506 <whoami90506@github.com>
2019-05-26 20:56:09 +0800

    edit structure

--- hardware/src/PEArrayController.v
+++ hardware/src/PEArrayController.v
@@ -16,7 +16,6 @@ module PEArrayController (
 
 	//top && controll
 	input i_start,
-	output reg o_busy,
 	output [`V_E_F_Bit-1:0] o_result,
 	output reg o_valid,
 
@@ -37,7 +36,7 @@ module PEArrayController (
 	input [`V_E_F_Bit-1:0] i_v_a,
 	input [`V_E_F_Bit-1 : 0] i_f,
 	input i_t_newline,
-	input i_enable_0,
+	input i_enable_0
 );
 
 genvar idx;
@@ -81,246 +80,246 @@ wire [`V_E_F_Bit-1 :0] PE_f [0 : `PE_Array_size];
 //Max
 wire [`V_E_F_Bit * `PE_Array_size -1 : 0] PE_v_1D;
 
-//assignment
-assign PE_t[0] = t_buf;
-assign PE_v[0] = v_buf;
-assign PE_v_a[0] = v_a_buf;
-assign PE_f[0] = f_buf;
-assign PE_newline[0] = newline_buf;
+// //assignment
+// assign PE_t[0] = t_buf;
+// assign PE_v[0] = v_buf;
+// assign PE_v_a[0] = v_a_buf;
+// assign PE_f[0] = f_buf;
+// assign PE_newline[0] = newline_buf;
 
-assign n_v_a_buf = i_v + i_minusAlpha;
-assign n_o_t = PE_t[{1'b0, s_using} +1];
-assign n_o_v = PE_v[{1'b0, s_using} +1];
-assign n_o_f = PE_f[{1'b0, s_using} +1];
+// assign n_v_a_buf = i_v + i_minusAlpha;
+// assign n_o_t = PE_t[{1'b0, s_using} +1];
+// assign n_o_v = PE_v[{1'b0, s_using} +1];
+// assign n_o_f = PE_f[{1'b0, s_using} +1];
 
 
-//control
-always @(*) begin
-	n_state = state;
-	n_counter = counter;
-	n_first_itr = first_itr;
+// //control
+// always @(*) begin
+// 	n_state = state;
+// 	n_counter = counter;
+// 	n_first_itr = first_itr;
 
-	case (state)
-		IDLE : begin
-			if(i_start)n_state = READ_ST;
-			n_counter = 0;
-			n_first_itr = 1'b1;
-		end //IDLE
+// 	case (state)
+// 		IDLE : begin
+// 			if(i_start)n_state = READ_ST;
+// 			n_counter = 0;
+// 			n_first_itr = 1'b1;
+// 		end //IDLE
 
-		READ_ST : begin
-			if(i_data_valid) begin
-				if(i_s_last | i_t_last | (counter == s_using) ) begin
-					n_counter = 0;
-					n_first_itr = 1'b0;
-				end else n_counter = counter +1;
+// 		READ_ST : begin
+// 			if(i_data_valid) begin
+// 				if(i_s_last | i_t_last | (counter == s_using) ) begin
+// 					n_counter = 0;
+// 					n_first_itr = 1'b0;
+// 				end else n_counter = counter +1;
 
-				case ({i_s_last, i_t_last})
-					2'b11 : n_state = WAIT;
-					2'b10 : n_state = FINAL_T;
-					2'b01 : n_state = READ_ST;
-					2'b00 : begin
-						if (counter == s_using) n_state = READ_T;
-					end
+// 				case ({i_s_last, i_t_last})
+// 					2'b11 : n_state = WAIT;
+// 					2'b10 : n_state = FINAL_T;
+// 					2'b01 : n_state = READ_ST;
+// 					2'b00 : begin
+// 						if (counter == s_using) n_state = READ_T;
+// 					end
 					
-				endcase
-			end
-		end //READ_ST
+// 				endcase
+// 			end
+// 		end //READ_ST
 
-		READ_T : if(i_data_valid & i_t_last) n_state = READ_ST;
+// 		READ_T : if(i_data_valid & i_t_last) n_state = READ_ST;
 
-		FINAL_T : if(i_data_valid & i_t_last) n_state = WAIT;
+// 		FINAL_T : if(i_data_valid & i_t_last) n_state = WAIT;
 
-		WAIT : if(PE_enable == 0) n_state = RESULT;
+// 		WAIT : if(PE_enable == 0) n_state = RESULT;
 
-		RESULT : n_state = IDLE;
-	endcase
-end
+// 		RESULT : n_state = IDLE;
+// 	endcase
+// end
 
-//IO
-always @(*) begin
-	n_o_busy = 1'b1;
-	n_o_valid = 1'b0;
-	n_t_valid_buf = 1'b0;
-	o_update_s_w = 1'b0;
-	o_update_t_w = 1'b0;
+// //IO
+// always @(*) begin
+// 	n_o_busy = 1'b1;
+// 	n_o_valid = 1'b0;
+// 	n_t_valid_buf = 1'b0;
+// 	o_update_s_w = 1'b0;
+// 	o_update_t_w = 1'b0;
 	
 
-	case (state)
-		IDLE : begin
-			n_o_busy = i_start;
-			o_update_s_w = 1'b1;
-			o_update_t_w = 1'b1;
-		end//IDLE
+// 	case (state)
+// 		IDLE : begin
+// 			n_o_busy = i_start;
+// 			o_update_s_w = 1'b1;
+// 			o_update_t_w = 1'b1;
+// 		end//IDLE
 
-		READ_ST : begin
-			o_update_s_w = 1'b1;
-			o_update_t_w = 1'b1;
+// 		READ_ST : begin
+// 			o_update_s_w = 1'b1;
+// 			o_update_t_w = 1'b1;
 
-			if(i_data_valid) begin
-				n_t_valid_buf = ~first_itr;
-				case ({i_s_last, i_t_last})
-					2'b11 : begin //WAIT
-						o_update_t_w = 1'd0;
-						o_update_s_w = 1'd0;
-					end 
-					2'b10 : begin // final T
-						o_update_t_w = 1'd1;
-						o_update_s_w = 1'd0;
-					end
-					2'b00 : begin
-						if (counter == s_using) begin //READ_T
-							o_update_t_w = 1'd1;
-							o_update_s_w = 1'd0;
-						end
-					end
+// 			if(i_data_valid) begin
+// 				n_t_valid_buf = ~first_itr;
+// 				case ({i_s_last, i_t_last})
+// 					2'b11 : begin //WAIT
+// 						o_update_t_w = 1'd0;
+// 						o_update_s_w = 1'd0;
+// 					end 
+// 					2'b10 : begin // final T
+// 						o_update_t_w = 1'd1;
+// 						o_update_s_w = 1'd0;
+// 					end
+// 					2'b00 : begin
+// 						if (counter == s_using) begin //READ_T
+// 							o_update_t_w = 1'd1;
+// 							o_update_s_w = 1'd0;
+// 						end
+// 					end
 					
-					//2'b01 remain the same
-				endcase
-			end		
-		end//READ_ST
+// 					//2'b01 remain the same
+// 				endcase
+// 			end		
+// 		end//READ_ST
 
-		READ_T : begin
-			o_update_t_w = 1'd1;
-			o_update_s_w = 1'd0;
+// 		READ_T : begin
+// 			o_update_t_w = 1'd1;
+// 			o_update_s_w = 1'd0;
 
-			if(i_data_valid) begin
-				n_t_valid_buf = 1'd1;
-				if(i_t_last) o_update_s_w = 1'b1;
-			end
-		end //READ_T
+// 			if(i_data_valid) begin
+// 				n_t_valid_buf = 1'd1;
+// 				if(i_t_last) o_update_s_w = 1'b1;
+// 			end
+// 		end //READ_T
 
-		FINAL_T : begin
-			o_update_t_w = ~(i_data_valid & i_t_last);
-			o_update_s_w = 1'd0;
-		end //FINAL_T
+// 		FINAL_T : begin
+// 			o_update_t_w = ~(i_data_valid & i_t_last);
+// 			o_update_s_w = 1'd0;
+// 		end //FINAL_T
 
-		//Wait is default
+// 		//Wait is default
 
-		RESULT : n_o_valid = 1'd1;
-	endcase
-end
+// 		RESULT : n_o_valid = 1'd1;
+// 	endcase
+// end
 
-//PE
-always @(*) begin
-	n_newline = newline;
-	n_PE_lock = ~i_data_valid;
-	n_s_using = s_using;
-	for(i = 0; i < `PE_Array_size; i = i+1) begin
-		n_PE_enable[i] = PE_enable[i];
-		n_PE_s[i] = PE_s[i];
-	end
+// //PE
+// always @(*) begin
+// 	n_newline = newline;
+// 	n_PE_lock = ~i_data_valid;
+// 	n_s_using = s_using;
+// 	for(i = 0; i < `PE_Array_size; i = i+1) begin
+// 		n_PE_enable[i] = PE_enable[i];
+// 		n_PE_s[i] = PE_s[i];
+// 	end
 
-	case (state)
-		IDLE : begin
-			n_newline = 1'b1;
-			n_PE_lock = 1'b1;
-		end //IDLE
+// 	case (state)
+// 		IDLE : begin
+// 			n_newline = 1'b1;
+// 			n_PE_lock = 1'b1;
+// 		end //IDLE
 
-		READ_ST : begin
-			if(i_data_valid) begin
-				n_PE_s[counter] = i_s;
-				n_PE_enable[counter] = 1'd1;
-				n_newline = i_t_last;
-				if(i_s_last) n_s_using = counter;
-			end
-		end //READ_ST
+// 		READ_ST : begin
+// 			if(i_data_valid) begin
+// 				n_PE_s[counter] = i_s;
+// 				n_PE_enable[counter] = 1'd1;
+// 				n_newline = i_t_last;
+// 				if(i_s_last) n_s_using = counter;
+// 			end
+// 		end //READ_ST
 
-		READ_T : begin
-			if(i_data_valid) n_newline = i_t_last;
-		end //READ_T
+// 		READ_T : begin
+// 			if(i_data_valid) n_newline = i_t_last;
+// 		end //READ_T
 
-		FINAL_T : begin
-			if(i_data_valid) begin
-				if(s_using < `PE_Array_size -1) n_PE_enable[s_using +1] = 1'd0;
+// 		FINAL_T : begin
+// 			if(i_data_valid) begin
+// 				if(s_using < `PE_Array_size -1) n_PE_enable[s_using +1] = 1'd0;
 
-				for(i = 0; i < `PE_Array_size; i = i+1) begin
-					if( (i > s_using) & (~PE_enable[i-1]) )n_PE_enable[i] = 1'd0;
-				end
-			end
-		end //FINAL_T
+// 				for(i = 0; i < `PE_Array_size; i = i+1) begin
+// 					if( (i > s_using) & (~PE_enable[i-1]) )n_PE_enable[i] = 1'd0;
+// 				end
+// 			end
+// 		end //FINAL_T
 
-		WAIT : begin
-			n_PE_lock = 1'd0;
+// 		WAIT : begin
+// 			n_PE_lock = 1'd0;
 
-			n_PE_enable[0] = 1'd0;
-			n_PE_enable[s_using+1] = 1'd0;
-			for(i = 1; i < `PE_Array_size; i = i+1) begin
-				if(~PE_enable[i-1]) n_PE_enable[i] = 1'd0;
-			end
-		end //WAIT
+// 			n_PE_enable[0] = 1'd0;
+// 			n_PE_enable[s_using+1] = 1'd0;
+// 			for(i = 1; i < `PE_Array_size; i = i+1) begin
+// 				if(~PE_enable[i-1]) n_PE_enable[i] = 1'd0;
+// 			end
+// 		end //WAIT
 
-		RESULT : begin
-			n_newline = 1'd1;
-			n_PE_lock = 1'd1;
-			n_s_using = `PE_Array_size -1;
-			for(i = 0; i < `PE_Array_size; i = i+1) n_PE_enable[i] = 1'd0;
-		end // RESULT
-	endcase
-end
+// 		RESULT : begin
+// 			n_newline = 1'd1;
+// 			n_PE_lock = 1'd1;
+// 			n_s_using = `PE_Array_size -1;
+// 			for(i = 0; i < `PE_Array_size; i = i+1) n_PE_enable[i] = 1'd0;
+// 		end // RESULT
+// 	endcase
+// end
 
-always @(posedge clk or negedge rst_n) begin
-	if (~rst_n) begin
-		//control
-		state <= IDLE;
-		counter <= 0;
-		first_itr <= 1'd1;
+// always @(posedge clk or negedge rst_n) begin
+// 	if (~rst_n) begin
+// 		//control
+// 		state <= IDLE;
+// 		counter <= 0;
+// 		first_itr <= 1'd1;
 
-		//IO
-		o_busy <= 1'd0;
-		o_valid <= 1'd0;
-		o_t <= 2'd0;
-		o_v <= 0;
-		o_f <= 0;
-		o_t_valid <= 1'd0;
-		t_buf <= 2'd0;
-		v_buf <= 0;
-		f_buf <= 0;
-		v_a_buf <= 0;
-		t_valid_buf <= 1'b0;
-		t_valid_buf1 <= 1'b0;
+// 		//IO
+// 		o_busy <= 1'd0;
+// 		o_valid <= 1'd0;
+// 		o_t <= 2'd0;
+// 		o_v <= 0;
+// 		o_f <= 0;
+// 		o_t_valid <= 1'd0;
+// 		t_buf <= 2'd0;
+// 		v_buf <= 0;
+// 		f_buf <= 0;
+// 		v_a_buf <= 0;
+// 		t_valid_buf <= 1'b0;
+// 		t_valid_buf1 <= 1'b0;
 
-		//PE
-		newline <= 1'd1;
-		newline_buf <= 1'd1;
-		s_using <= `PE_Array_size -1;
-		PE_lock <= 1'd0;
+// 		//PE
+// 		newline <= 1'd1;
+// 		newline_buf <= 1'd1;
+// 		s_using <= `PE_Array_size -1;
+// 		PE_lock <= 1'd0;
 
-		for(i = 0; i < `PE_Array_size; i = i+1) begin
-			PE_enable[i] <= 1'b0;
-			PE_s[i] <= 2'd0;
-		end
-	end else begin
-		//control
-		state <= n_state;
-		counter <= n_counter;
-		first_itr <= n_first_itr;
+// 		for(i = 0; i < `PE_Array_size; i = i+1) begin
+// 			PE_enable[i] <= 1'b0;
+// 			PE_s[i] <= 2'd0;
+// 		end
+// 	end else begin
+// 		//control
+// 		state <= n_state;
+// 		counter <= n_counter;
+// 		first_itr <= n_first_itr;
 
-		//IO
-		o_busy <= n_o_busy;
-		o_valid <= n_o_valid;
-		o_t <= n_o_t;
-		o_v <= n_o_v;
-		o_f <= n_o_f;
-		o_t_valid <= t_valid_buf1;
-		t_buf <= i_t;
-		v_buf <= i_v;
-		f_buf <= i_f;
-		v_a_buf <= n_v_a_buf;
-		t_valid_buf1 <= t_valid_buf;
-		t_valid_buf <= n_t_valid_buf;
+// 		//IO
+// 		o_busy <= n_o_busy;
+// 		o_valid <= n_o_valid;
+// 		o_t <= n_o_t;
+// 		o_v <= n_o_v;
+// 		o_f <= n_o_f;
+// 		o_t_valid <= t_valid_buf1;
+// 		t_buf <= i_t;
+// 		v_buf <= i_v;
+// 		f_buf <= i_f;
+// 		v_a_buf <= n_v_a_buf;
+// 		t_valid_buf1 <= t_valid_buf;
+// 		t_valid_buf <= n_t_valid_buf;
 
-		//PE
-		newline <= n_newline;
-		newline_buf <= newline;
-		s_using <= n_s_using;
-		PE_lock <= n_PE_lock;
+// 		//PE
+// 		newline <= n_newline;
+// 		newline_buf <= newline;
+// 		s_using <= n_s_using;
+// 		PE_lock <= n_PE_lock;
 
-		for(i = 0; i < `PE_Array_size; i = i+1) begin
-			PE_enable[i] <= n_PE_enable[i];
-			PE_s[i] <= n_PE_s[i];
-		end
-	end
-end
+// 		for(i = 0; i < `PE_Array_size; i = i+1) begin
+// 			PE_enable[i] <= n_PE_enable[i];
+// 			PE_s[i] <= n_PE_s[i];
+// 		end
+// 	end
+// end
 
 generate
 	for(idx = 0; idx < `PE_Array_size; idx = idx+1) PE PE_cell(.clk(clk), .rst(rst_n), .enable(PE_enable[idx]), 
--- hardware/src/top.v
+++ hardware/src/top.v
@@ -59,19 +59,23 @@ reg [`V_E_F_Bit-1 : 0] n_post_mismatch, n_post_alpha, n_post_beta;
 
 //SramController
 wire sram_busy;
-wire [`Sram_Word-1:0] data_dp_to_sram;
+wire [`Sram_Word-1:0] data_sram_to_dp;
 wire [`Max_T_size_log-1 : 0] t_size;
 
 //DataProcessor
 wire dp_request_sram, dp_store_sram; 
-wire [`Sram_Word-1:0] data_sram_to_dp;
-wire dp_data_valid, seq_s_last, seq_t_last;
-wire [1:0] seq_s, t_dp_to_PE;
-wire [`V_E_F_Bit-1 : 0] v_dp_to_PE, f_dp_to_PE;
+wire [`Sram_Word-1:0] data_dp_to_sram;
+wire dp_busy;
+
+wire PE_lock, PE_enable_0, PE_newline;
+wire seq_s_last;
+wire [`PE_Array_size*2-1:0] seq_s;
+wire [`PE_Array_size_log-1 : 0] seq_s_addr;
+wire [1:0] t_dp_to_PE;
+wire [`V_E_F_Bit-1 : 0] v_dp_to_PE, f_dp_to_PE, v_a_dp_to_PE;
 
 //PEArrayController
-wire PE_busy, t_valid;
-wire PE_update_s_w, PE_update_t_w;
+wire t_valid;
 wire [1:0] t_PE_to_dp;
 wire [`V_E_F_Bit-1 : 0] v_PE_to_dp, f_PE_to_dp;
 
@@ -107,8 +111,8 @@ always @(*) begin
 		end
 
 		RESET : begin
-			if(~sram_busy & ~PE_busy )n_state = IDLE;
-			n_o_busy = sram_busy | PE_busy;
+			if(~sram_busy & ~dp_busy )n_state = IDLE;
+			n_o_busy = sram_busy | dp_busy;
 		end
 	endcase
 end
@@ -186,15 +190,15 @@ SramController mem(.clk(clk), .rst_n(rst_n), .i_PE_request(dp_request_sram), .o_
 	.i_send_data(data_dp_to_sram), .o_T_size(t_size), .i_init(o_valid), .i_start_read_t(start_read_t), .i_t(_t), .o_busy(sram_busy));
 
 DataProcessor dp(.clk(clk), .rst_n(rst_n), .o_sram_request(dp_request_sram), .i_request_data(data_sram_to_dp), .o_sram_send(dp_store_sram), 
-	.o_send_data(data_dp_to_sram), .i_T_size(t_size), .o_valid(dp_data_valid), .i_init(o_valid), .i_PE_update_s(PE_update_s_w), .o_s(seq_s), 
-	.o_s_last(seq_s_last), .i_PE_update_t(PE_update_t_w), .o_t(t_dp_to_PE), .o_v(v_dp_to_PE), .o_f(f_dp_to_PE), .i_t(t_PE_to_dp), 
-	.i_v(v_PE_to_dp), .i_f(f_PE_to_dp), .o_t_last(seq_t_last), .o_request_s(o_request_s), .i_s(_s), .i_s_valid(_s_valid), 
-	.i_t_valid(t_valid), .i_start_calc(start_cal));
+	.o_send_data(data_dp_to_sram), .i_T_size(t_size), .o_lock(PE_lock), .i_init(o_valid), .o_s(seq_s), .o_s_last(seq_s_last), 
+	.o_t(t_dp_to_PE), .o_v(v_dp_to_PE), .o_v_a(v_a_dp_to_PE), .o_f(f_dp_to_PE), .o_t_newline(PE_newline), .o_enable_0(PE_enable_0), 
+	.i_t(t_PE_to_dp), .i_v(v_PE_to_dp), .i_f(f_PE_to_dp), .o_request_s(o_request_s), .i_s(_s), .i_s_valid(_s_valid), 
+	.i_t_valid(t_valid), .i_start_calc(start_cal), .o_busy(dp_busy), .o_s_addr(seq_s_addr));
 
 PEArrayController PE(.clk(clk), .rst_n(rst_n), .i_match(post_match), .i_mismatch(post_mismatch), .i_minusAlpha(post_alpha), 
-	.i_minusBeta (post_beta), .i_start(start_cal), .o_busy(PE_busy), .o_result(o_result), .o_valid(o_valid), .i_data_valid(dp_data_valid), 
-	.o_update_s_w(PE_update_s_w), .i_s(seq_s), .i_s_last(seq_s_last), .o_update_t_w(PE_update_t_w), .o_t(t_PE_to_dp), .o_v(v_PE_to_dp), 
-	.o_f(f_PE_to_dp), .i_t(t_dp_to_PE), .i_v(v_dp_to_PE), .i_f(f_dp_to_PE), .i_t_last(seq_t_last), .o_t_valid(t_valid));
+	.i_minusBeta (post_beta), .i_start(start_cal), .o_result(o_result), .o_valid(o_valid), .i_s(seq_s), .i_s_last(seq_s_last), 
+	.i_s_addr(seq_s_addr), .o_t(t_PE_to_dp), .o_v(v_PE_to_dp), .o_f(f_PE_to_dp), .i_t(t_dp_to_PE), .i_v(v_dp_to_PE), 
+	.i_v_a(v_a_dp_to_PE), .i_f(f_dp_to_PE), .i_t_newline(PE_newline), .i_enable_0(PE_enable_0), .o_t_valid(t_valid), .i_lock(PE_lock));
 
 endmodule
 `endif
