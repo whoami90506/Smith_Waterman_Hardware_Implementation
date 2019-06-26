8ed113560cbafc8d2ec4d1e00e9c820d20432972
whoami90506 <whoami90506@github.com>
2019-06-27 04:02:59 +0800

    add 10.0

--- hardware/testbench/testfixture.v
+++ hardware/testbench/testfixture.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ps
-`define CYCLE    20.0           	        // Modify your clock period here
+`define CYCLE    10.0           	        // Modify your clock period here
 `define TERMINATION  50000000
 
 `include "src/util.v"
